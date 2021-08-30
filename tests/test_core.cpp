6adf2ad036a7aa68ac8706a4f1598022bcd816a1
David Summers <david@summersoft.fay-ar.us>
2021-08-29 20:04:59 -0500

    Start moving tests out of demo program in to tests/test_core.cpp which builds into test_deranged1 program at the moment (to be changed once I move all tests out of demo program).

--- tests/test_core.cpp
+++ tests/test_core.cpp
@@ -24,8 +24,10 @@
 
 #include "test.h"
 
+#include "dranged/core/algorithms.h"
 #include "dranged/core/border.h"
 #include "dranged/core/element.h"
+#include "dranged/core/range.h"
 
 using namespace dranged;
 
@@ -49,16 +51,103 @@ static error_t test_border( )
 
 static error_t test_element( )
 {
+  // Test Element.
+  // At the moment, just make sure we can instantiate it.
+  IntVec myVec { 1, 2, 3, 4, 5 };
+
+  Element< IntVec > element0;
+  Element< IntVec > element1( myVec.begin( ) );
+  Element< IntVec > element2( myVec.end(   ) );
 
   return NO_ERROR;
 }
 
+static error_t test_range_for_each( )
+{
+  IntVec myVec { 1, 2, 3, 4, 5 };
+  Range myRange( myVec );
+  IntVec result;
+
+  // Test 'for_each'.
+  for_each( myRange, [ & ] ( int &val_ )
+  {
+    result.push_back( val_ );
+  } );
+
+  if ( myVec != result )
+  {
+    ERROR( "Result vector didn't match original vector." );
+  }
+
+  return NO_ERROR;
+}
+
+static error_t test_range_find( )
+{
+  IntVec myVec { 1, 2, 3, 4, 5 };
+  Range myRange( myVec );
+
+  // Test 'find'.
+  if ( auto element = find( myRange, 3 ); *element != 3 )
+  {
+    ERROR( "Element '3' was not at position 3." );
+  }
+
+  return NO_ERROR;
+}
+
+static error_t test_range_find_if( )
+{
+  IntVec myVec { 1, 2, 3, 4, 5 };
+  Range myRange( myVec );
+
+  // Test found.
+  if ( auto element = find_if( myRange, [ ] ( int &value_ )
+                      {
+                        return value_ == 4;
+                      } ); *element != 4 )
+  {
+    ERROR( "Element '4' was not at position 4." );
+  }
+
+  // Test not found.
+  if ( auto element = find_if( myRange, [ ] ( int &value_ )
+                      {
+                        return value_ == 6;
+                      } ) )
+  {
+    ERROR( "Error: Element value 6 was found but should not have been found." );
+  }
+
+  return NO_ERROR;
+}
+
+static error_t test_range_find_if_not( )
+{
+  IntVec myVec { 1, 2, 3, 4, 5 };
+  Range myRange( myVec );
+
+  if ( auto element = find_if_not( myRange, [ ] ( int &value_ )
+                      {
+                        return value_ == 7;
+                      } ); !element )
+  {
+    ERROR( "Error: Element '7' found when it should not have been." );
+  }
+
+  return NO_ERROR;
+}
 
 /* The test table.  */
 
 struct test_descriptor_t test_funcs[] =
 {
   TEST_NULL,
-  TEST_PASS( test_border, "Basic Border Tests" ),
+  TEST_PASS( test_border,            "Basic Border Tests" ),
+  TEST_PASS( test_element,           "Basic Element Tests" ),
+  TEST_PASS( test_range_for_each,    "Basic Range for_each Tests" ),
+  TEST_PASS( test_range_find,        "Basic Range find Tests" ),
+  TEST_PASS( test_range_find_if,     "Basic Range find_if Tests" ),
+  TEST_PASS( test_range_find_if_not, "Basic Range find_if_not Tests" ),
   TEST_NULL
 };
